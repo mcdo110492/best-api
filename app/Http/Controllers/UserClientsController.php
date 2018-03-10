7195e0b5a026043d7bbc241846083da8fc1d6825
fuentebellamcdonald@gmail.com <fuentebellamcdonald@gmail.com>
2018-03-10 10:59:41 +0800

    Change to SparkPost

--- app/Http/Controllers/UserClientsController.php
+++ app/Http/Controllers/UserClientsController.php
@@ -178,7 +178,7 @@ class UserClientsController extends Controller
 
     public function sendMail(){
 
-       $mail = Mail::to("mcdonaldclintonfuentebella@gmail.com")->send(new AccountConfirmation());
+       $mail = Mail::to("fuentebellamcdonald@gmail.com")->send(new AccountConfirmation());
 
         return response()->json(['status' => 200, 'mail' => $mail]);
 
